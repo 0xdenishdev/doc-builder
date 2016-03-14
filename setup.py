1d3ea75eb08c0e109981c12949b58349f21f27b6
Denis.Horobtsov <gninjava@gmail.com>
2016-03-14 22:58:39 +0200

    added setup file. V-007

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='docbuilder',
+    version='0.1',
+    description='Simple documentation builder',
+    url='http://github.com/gninjava/doc-builder',
+    author='Denis Horobtsov',
+    author_email='denishorobtsov@gmail.com',
+    license='MIT',
+    packages=['docbuilder'],
+    zip_safe=False
+)
